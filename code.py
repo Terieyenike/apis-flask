d8c0efdd049fc568b2eab4a1bb44c05277a9e3f2
Teri Eyenike <teyenike1@gmail.com>
2023-10-23 02:26:56 +0100

    Update: code

--- code.py
+++ code.py
@@ -3,7 +3,9 @@ import requests
 url = "http://127.0.0.1:5000/friend"
 
 payload={}
-headers = {}
+headers = {
+  'Accept': 'application/json'
+}
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
